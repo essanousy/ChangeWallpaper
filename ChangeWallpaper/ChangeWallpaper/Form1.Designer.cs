971670f4fad53c2a4a3750ba25aa7e5ac1a43047
essanousy <mohamed.essanousy@gmail.com>
2017-05-25 11:31:29 +0000

    First commit

---
+++ ChangeWallpaper/ChangeWallpaper/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ChangeWallpaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.NUDTimer = new System.Windows.Forms.NumericUpDown();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblimg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDTimer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Timer Interval";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(248, 78);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // NUDTimer
+            // 
+            this.NUDTimer.Location = new System.Drawing.Point(92, 13);
+            this.NUDTimer.Maximum = new decimal(new int[] {
+            20000,
+            0,
+            0,
+            0});
+            this.NUDTimer.Name = "NUDTimer";
+            this.NUDTimer.Size = new System.Drawing.Size(150, 20);
+            this.NUDTimer.TabIndex = 3;
+            this.NUDTimer.ThousandsSeparator = true;
+            this.NUDTimer.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(329, 78);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "D:\\Personal\\Code\\SetWallpaper\\SetWallpaper\\bin\\Debug\\Wallpaper";
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "D:\\Personal\\Code\\SetWallpaper\\SetWallpaper\\bin\\Debug\\Wallpaper";
+            this.textBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(130, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Change Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ChangeWallpaper.Properties.Resources.ajax_loader__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 34);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblimg
+            // 
+            this.lblimg.AutoSize = true;
+            this.lblimg.Location = new System.Drawing.Point(300, 15);
+            this.lblimg.Name = "lblimg";
+            this.lblimg.Size = new System.Drawing.Size(23, 13);
+            this.lblimg.TabIndex = 8;
+            this.lblimg.Text = "img";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 102);
+            this.Controls.Add(this.lblimg);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.NUDTimer);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Change Wallpaper";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.NUDTimer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown NUDTimer;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblimg;
+    }
+}
+
---
+++ ChangeWallpaper/ChangeWallpaper/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ChangeWallpaper
+{
+    public partial class Form1 : Form
+    {
+        private ContextMenu m_menu;  
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_menu = new ContextMenu();
+            m_menu.MenuItems.Add(0, new MenuItem("Show", new System.EventHandler(Show_Click)));
+            m_menu.MenuItems.Add(1, new MenuItem("Hide", new System.EventHandler(Hide_Click)));
+            m_menu.MenuItems.Add(2, new MenuItem("Exit", new System.EventHandler(Exit_Click)));
+
+            m_menu.MenuItems.Add(1, new MenuItem("Change Wallpaper", new System.EventHandler(ChangeWallpaper_Click)));
+
+            notifyIcon1.ContextMenu = m_menu;
+
+        }
+        protected void Exit_Click(Object sender, System.EventArgs e)
+        {
+            Close();
+        }
+        protected void Hide_Click(Object sender, System.EventArgs e)
+        {
+            Hide();
+        }
+        protected void Show_Click(Object sender, System.EventArgs e)
+        {
+            Show();
+        }
+        protected void ChangeWallpaper_Click(Object sender, System.EventArgs e)
+        {
+            ChangeWallaperRandom();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Minimized";
+            notifyIcon1.BalloonTipText = "to show form double click on icon";
+
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ChangeWallaperRandom();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int sec = Convert.ToInt16(NUDTimer.Value) * 1000;
+            timer1.Interval = sec;
+            timer1.Enabled = true; 
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false; 
+        }
+
+        private void textBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            //textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeWallaperRandom();
+        }
+        public void ChangeWallaperRandom() { 
+        pictureBox1.Visible = true;
+            Random rnd = new Random();
+            DirectoryInfo dr = new DirectoryInfo(textBox1.Text);
+            FileInfo[] files = dr.GetFiles();
+            int num = rnd.Next(0, files.Count() - 1);
+            string file = files[num].Name;
+            lblimg.Text = file;
+            string FilePath = textBox1.Text + @"\" + file;
+            Wallpaper.Set(FilePath, Wallpaper.Style.Stretched);
+
+            pictureBox1.Visible = false;
+        }
+    }
+}
